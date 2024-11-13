5095ba71246bc3206dd9919881324c1689848a73
LauraKirby <laurakirby26@gmail.com>
2024-11-12 22:17:41 -0800

    remove java backend

--- player-service-backend/src/main/java/com/app/playerservicejava/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.playerservicejava.repository;
-import com.app.playerservicejava.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, String> {
-
-}
