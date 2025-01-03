d7351199c83201ac559fd8343d436fd8b1cd0622
Sriram <organicmistakes@gmail.com>
2025-01-03 15:24:56 -0500

    tes

--- players-ui-react/src/components/PlayersResults.jsx
+++ players-ui-react/src/components/PlayersResults.jsx
@@ -1,43 +1,50 @@
 import React, { useEffect, useState } from 'react';
 
-import {validateId, validateCountryCode, santifizeInput} from "../utils";
-import useDatatFetcher, { fetchPostData } from "../utils/DataFetcher";
-import fetchData from "../utils/DataFetcher";
+import {validateId, validateCountryCode} from "../utils";
+import fetchData, { postData } from "../utils/DataFetcher";
 
 function PlayerResults() {
 
     const [players, setPlayers] = useState([]);
     const [inputID, setInputID] = useState('');
+    const [country, setCountry] = useState('');
 
 
     useEffect(() => {
         playerFetch(null, 'players');
     }, []);
 
-    const playerFetch = (ep, prop) => {
-        fetchData(ep)
-        .then(data => {
-            const subsetOfPlayers = data[prop].slice(0,10);
-            setPlayers(subsetOfPlayers)
-            console.log(subsetOfPlayers)
-        })
+    const playerFetch = async (ep, prop) => {
+        const data = await fetchData(ep);  
+        const subsetOfPlayers = data[prop].slice(0,10);
+        setPlayers(subsetOfPlayers);
     }
 
-    const handleSearchById = (input) => {
-
+    const handleSearchById = async (input) => {
         if (validateId(input)) {
             // do something
-            playerFetch(`/v1/players/${input}`, 'player');
+            await playerFetch(`/v1/players/${input}`, 'player');
         }
     }
 
-    const handleSearchByCountry = (input) => {
+    const handleSearchByCountry = async (input) => {
 
         if (validateCountryCode(input)) {
             // do something
-            fetchPostData();
+            await playerFetch(`/v1/players/${input}`, 'player');
         }
     }
+
+    const generateTeam = async (bodyInput) => {
+        try{
+        const data = await postData(bodyInput);
+        setPlayers(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+          } finally {
+          }
+    }
+    
 
  return (
      <div className="player-results">
@@ -49,10 +56,17 @@ function PlayerResults() {
              </div>
              <div className="player-results-search">
                  <label >Player Country Code:</label>
-                 <input type=""/>
-                 <button onClick={()=>handleSearchByCountry()}>Submit</button>
+                 <input type="text" value={country} onChange={(e) => setCountry(e.target.value)}/>
+                 <button onClick={()=>handleSearchByCountry(country)}>Submit</button>
              </div>
          </div>
+         { (inputID ||  country )=== '' ?
+            <div className="player-results-generate-team">
+            <button onClick={(e)=>generateTeam({"seed_id":"abbotji01","team_size":10})}>GenerateTeam</button>
+            </div>: 
+            ''
+        }
+        
          <div className="players-results-section">
              {/* Body of results should go here */}
              {players.map((player) => {
